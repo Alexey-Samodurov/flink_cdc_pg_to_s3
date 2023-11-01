8993a7c139bef45d530f25549e3b71a7e7bc5644
Alexey <70944468+Alexey-Samodurov@users.noreply.github.com>
2023-11-01 17:36:39 +0300

    Update run_kubectl.sh

--- run_kubectl.sh
+++ run_kubectl.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 #kubectl create -f flink-configuration-configmap.yaml
-kubectl create -f flink-master-deploy.yaml
-kubectl create -f flink-worker-deploy.yaml
-kubectl create -f flink-master-service.yaml
+kubectl apply -f flink-master-deploy.yaml
+kubectl apply -f flink-worker-deploy.yaml
+#kubectl apply -f flink-rest-deploy.yaml
+kubectl apply -f flink-master-service.yaml
+#kubectl apply -f flink-api-service.yaml
