75e23a51df603d2b894813c945604199f9d99d44
Alexey <70944468+Alexey-Samodurov@users.noreply.github.com>
2023-11-03 16:23:32 +0300

    Update down_kubectl.sh

--- down_kubectl.sh
+++ down_kubectl.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 #kubectl delete -f flink-configuration-configmap.yaml
 kubectl delete -f flink-master-deploy.yaml
 kubectl delete -f flink-worker-deploy.yaml
-#kubectl delete -f flink-rest-deploy.yaml
-#kubectl delete -f flink-api-service.yaml
+kubectl delete -f flink-rest-deploy.yaml
+kubectl delete -f flink-api-service.yaml
 kubectl delete -f flink-master-service.yaml
